ab9be8d34627363e90cf61e9b10df157fc9a325a
Stanley Filipe <stanley92filipe@gmail.com>
2021-09-27 16:50:09 -0300

    update MainActivity.java

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Seja Bem Vindo!</Text>
+    </View>
+  );
+};
+
+export default App;
