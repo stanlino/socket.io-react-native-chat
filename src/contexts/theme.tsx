6795214216f1ee998e20816e0fff75b3fca55d72
Stanley Filipe <stanley92filipe@gmail.com>
2021-09-28 15:17:05 -0300

    fix theme context settings

--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
+import React, { createContext, SetStateAction, useContext, useState } from 'react'
 import { ThemeProvider as TP } from 'styled-components'
 import themes from '../themes'
 
+interface ThemeContextData {
+    toggleTheme(): any
+}
+
+const ThemeContext = createContext<ThemeContextData>({} as ThemeContextData)
+
 const ThemeProvider: React.FC = ({ children }) => {
 
-    const [ currentTheme, setCurrentTheme ] = useState('light')
+    const { dark, light } = themes
+ 
+    const [ theme, setTheme ] = useState(true)
 
-    const theme = currentTheme === 'light' ? themes.light : themes.dark
+    const toggleTheme = () => {
+        setTheme(!theme)
+    }
 
     return (
-        <TP theme={theme}>
-            {children}
-        </TP>
+        <ThemeContext.Provider value={{toggleTheme}}>
+            <TP theme={theme? light: dark }>
+                {children}
+            </TP>
+        </ThemeContext.Provider>
     )
 }
 
+export function useThemeContext() {
+    const { toggleTheme } = useContext(ThemeContext)
+    return toggleTheme
+}
+ 
 export default ThemeProvider
