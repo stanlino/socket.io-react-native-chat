0a31d6679aa3909aec7736164d9d6473a3427406
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-05 17:29:15 -0300

    update themes in app.routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ContactsPage from '../pages/Contacts'
 import SettingsPage from '../pages/Settings'
+import { ThemeContext } from 'styled-components/native'
 
 const BottomTabs = createBottomTabNavigator()
 
 const AppRoutes: React.FC = () => {
+  const { colors } = useContext(ThemeContext)
+
   return (
-    <BottomTabs.Navigator screenOptions={{ headerShown: false }}>
-      <BottomTabs.Screen component={ContactsPage} name={'contacts'} />
-      <BottomTabs.Screen component={SettingsPage} name={'settings'} />
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: { backgroundColor: colors.Secundary },
+        headerTitleStyle: { color: colors.Primary },
+      }}>
+      <BottomTabs.Screen
+        options={{ headerTitle: 'Contatos' }}
+        component={ContactsPage}
+        name={'contacts'}
+      />
+      <BottomTabs.Screen
+        options={{ headerTitle: 'Configurações' }}
+        component={SettingsPage}
+        name={'settings'}
+      />
     </BottomTabs.Navigator>
   )
 }
