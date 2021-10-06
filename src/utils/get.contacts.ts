905491360e62070aeaba5ee5eaee0f1a9a0cba7e
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-06 01:53:51 -0300

    create a method to save contacts from async storage

---
+++ src/utils/get.contacts.ts
@@ -0,0 +1,13 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+const getContacts = async () => {
+    try {
+        const jsonContacts = await AsyncStorage.getItem('@contacts')
+        return jsonContacts != null ? JSON.parse(jsonContacts) : [];
+    } catch (e) {
+        // saving error
+    }
+    
+}
+
+export default getContacts
