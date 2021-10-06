15e6f616d7f3e2e60db0155febe73fd5a4554b11
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-06 17:31:12 -0300

    update chat context

--- src/contexts/chat.tsx
+++ src/contexts/chat.tsx
@@ -6,30 +6,51 @@ interface ChatContextData {
   activeChat: Message[]
   updateActiveChat(chat: Message[]): void
   AddMessageToActiveChat(message: Message): void
+  activeContact: string
+  updateActiveContact(contact: string): void
 }
 
 const ChatContext = createContext<ChatContextData>({} as ChatContextData)
 
 const ChatProvider: React.FC = ({ children }) => {
   const [activeChat, setActiveChat] = useState<Message[]>([])
+  const [activeContact, setActiveContact] = useState('')
 
   const updateActiveChat = (chat: Message[]) => setActiveChat(chat)
+  const updateActiveContact = (contact: string) => setActiveContact(contact)
 
   const AddMessageToActiveChat = (message: Message) =>
     setActiveChat(prevState => [...prevState, message])
 
   return (
     <ChatContext.Provider
-      value={{ activeChat, updateActiveChat, AddMessageToActiveChat }}>
+      value={{
+        activeChat,
+        updateActiveChat,
+        AddMessageToActiveChat,
+        activeContact,
+        updateActiveContact,
+      }}>
       {children}
     </ChatContext.Provider>
   )
 }
 
 export const useChat = () => {
-  const { AddMessageToActiveChat, activeChat, updateActiveChat } =
-    useContext(ChatContext)
-  return { AddMessageToActiveChat, activeChat, updateActiveChat }
+  const {
+    AddMessageToActiveChat,
+    activeChat,
+    updateActiveChat,
+    activeContact,
+    updateActiveContact,
+  } = useContext(ChatContext)
+  return {
+    AddMessageToActiveChat,
+    activeChat,
+    updateActiveChat,
+    activeContact,
+    updateActiveContact,
+  }
 }
 
 export default ChatProvider
