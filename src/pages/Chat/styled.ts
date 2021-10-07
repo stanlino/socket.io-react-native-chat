a9f72331a44a6a7f0381bab2eb6e2bd107d1e6d0
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-07 14:21:05 -0300

    update ChatPage styles

--- src/pages/Chat/styled.ts
+++ src/pages/Chat/styled.ts
@@ -24,7 +24,7 @@ export const TextInput = styled.TextInput.attrs({
     border-radius: ${props => props.theme.sizes.ten}px;
     flex: 1;
     color: ${props => props.theme.colors.Primary};
-    font-size: ${props => props.theme.sizes.one * 5}px;
+    font-size: ${props => props.theme.sizes.one * 4}px;
     padding-left: ${props => props.theme.sizes.one * 3}px;
     padding-right: ${props => props.theme.sizes.one * 3}px;
     font-weight: 400;
@@ -49,7 +49,7 @@ export const MessageChildView = styled.View<MessageProps>`
     min-width: ${props => props.theme.sizes.ten * 1.5}px;
     border-radius: ${props => props.theme.sizes.ten}px;
     border-bottom-right-radius: ${props => props.fromThisUser ? 0 : props.theme.sizes.ten}px;
-    border-bottom-left-radius: ${props => props.fromThisUser ? props.theme.sizes.ten : 0}px;
+    border-top-left-radius: ${props => props.fromThisUser ? props.theme.sizes.ten : 0}px;
 `
 export const Text = styled.Text`
     font-size: ${props => props.theme.sizes.one * 5}px;
