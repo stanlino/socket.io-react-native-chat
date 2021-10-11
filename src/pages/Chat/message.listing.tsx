00042af1e55bdb47d89989e41afdfc18cca1a2fe
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-11 19:50:15 -0300

    update MessageListing Component

--- src/pages/Chat/message.listing.tsx
+++ src/pages/Chat/message.listing.tsx
@@ -1,5 +1,5 @@
 import { ScrollView } from 'react-native'
-import { MessageChildView, MessageFatherView, Text } from './styled'
+import { Container, MessageChildView, MessageFatherView, Text } from './styled'
 import { Message } from '../../utils/interfaces'
 import React, { useRef } from 'react'
 import { useUser } from '../../contexts/user'
@@ -13,22 +13,24 @@ const MessagesListing: React.FC<MessagesListingProps> = ({ messages }) => {
   const { userContact } = useUser()
 
   return (
-    <ScrollView
-      ref={scrollRef}
-      onContentSizeChange={() =>
-        scrollRef.current.scrollToEnd({ animated: true })
-      }>
-      {messages.map((message, index) => {
-        const fromThisUser = message.from === userContact
-        return (
-          <MessageFatherView fromThisUser={fromThisUser} key={index}>
-            <MessageChildView fromThisUser={fromThisUser}>
-              <Text>{message.message}</Text>
-            </MessageChildView>
-          </MessageFatherView>
-        )
-      })}
-    </ScrollView>
+    <Container>
+      <ScrollView
+        ref={scrollRef}
+        onContentSizeChange={() =>
+          scrollRef.current.scrollToEnd({ animated: true })
+        }>
+        {messages.map((message, index) => {
+          const fromThisUser = message.from === userContact
+          return (
+            <MessageFatherView fromThisUser={fromThisUser} key={index}>
+              <MessageChildView fromThisUser={fromThisUser}>
+                <Text>{message.message}</Text>
+              </MessageChildView>
+            </MessageFatherView>
+          )
+        })}
+      </ScrollView>
+    </Container>
   )
 }
 
