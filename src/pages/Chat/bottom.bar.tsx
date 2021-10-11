641603e540f2b9431c47cf40dda2c4a330fcc91e
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-11 19:50:30 -0300

    create a BottomBar component

---
+++ src/pages/Chat/bottom.bar.tsx
@@ -0,0 +1,57 @@
+import React, { useContext, useState } from 'react'
+import { ThemeContext } from 'styled-components/native'
+
+import { TextInput, BottomBar as BB, TouchableSend } from './styled'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import { Message } from '../../utils/interfaces'
+import { useSocket } from '../../contexts/socket'
+import { useUser } from '../../contexts/user'
+import { saveChat } from '../../utils/save.chat'
+
+interface BottomBarProps {
+  setMessages: React.Dispatch<React.SetStateAction<Message[]>>
+  contact_id: string
+}
+
+const BottomBar: React.FC<BottomBarProps> = ({ setMessages, contact_id }) => {
+  const { colors, sizes } = useContext(ThemeContext)
+  const { sendMessageToServer } = useSocket()
+  const { userContact } = useUser()
+
+  const [messageText, setMessageText] = useState('')
+
+  const sendMessage = async () => {
+    if (messageText !== '') {
+      const message = {
+        message: messageText,
+        to: contact_id,
+        from: userContact,
+      }
+      const send = await saveChat(message, contact_id)
+      if (send) {
+        setMessages(prevState => prevState.concat(message))
+        sendMessageToServer(message)
+        setMessageText('')
+      }
+    }
+  }
+
+  return (
+    <BB>
+      <TextInput
+        value={messageText}
+        onChangeText={text => setMessageText(text)}
+      />
+      <TouchableSend onPress={sendMessage}>
+        <Ionicons
+          name={'send'}
+          size={sizes.ten}
+          color={colors.Metallic_Seaweed}
+        />
+      </TouchableSend>
+    </BB>
+  )
+}
+
+export default BottomBar
--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,25 +1,16 @@
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/core'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StackParamList } from '../../routes/app.routes'
-import { Container, BottomBar, TextInput, TouchableSend } from './styled'
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import { ThemeContext } from 'styled-components/native'
 import { useChat } from '../../contexts/chat'
 import { getChat } from '../../utils/get.chat'
-import { useUser } from '../../contexts/user'
-import { useSocket } from '../../contexts/socket'
 import { Message } from '../../utils/interfaces'
-import { saveChat } from '../../utils/save.chat'
 import MessagesListing from './message.listing'
+import BottomBar from './bottom.bar'
 
 const ChatPage = () => {
   const { setOptions } = useNavigation()
   const { params } = useRoute<RouteProp<StackParamList, 'chat'>>()
-  const { colors, sizes } = useContext(ThemeContext)
   const { lastMessageReceived, setLastMessageReceived } = useChat()
-  const { userContact } = useUser()
-  const { sendMessageToServer } = useSocket()
-  const [messageText, setMessageText] = useState('')
   const [messages, setMessages] = useState<Message[]>([])
 
   useEffect(() => {
@@ -45,40 +36,10 @@ const ChatPage = () => {
     }
   }, [lastMessageReceived])
 
-  const sendMessage = async () => {
-    if (messageText !== '') {
-      const message = {
-        message: messageText,
-        to: params.contact_id,
-        from: userContact,
-      }
-      const send = await saveChat(message, params.contact_id)
-      if (send) {
-        setMessages(prevState => prevState.concat(message))
-        sendMessageToServer(message)
-        setMessageText('')
-      }
-    }
-  }
-
   return (
     <>
-      <Container>
-        <MessagesListing messages={messages} />
-      </Container>
-      <BottomBar>
-        <TextInput
-          value={messageText}
-          onChangeText={text => setMessageText(text)}
-        />
-        <TouchableSend onPress={sendMessage}>
-          <Ionicons
-            name={'send'}
-            size={sizes.ten}
-            color={colors.Metallic_Seaweed}
-          />
-        </TouchableSend>
-      </BottomBar>
+      <MessagesListing messages={messages} />
+      <BottomBar setMessages={setMessages} contact_id={params.contact_id} />
     </>
   )
 }
