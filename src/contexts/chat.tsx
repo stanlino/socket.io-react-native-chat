76e123676b64f3ff1d7d6846f021b7693f5e7873
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-11 19:11:05 -0300

    change the chat context strategy

--- src/contexts/chat.tsx
+++ src/contexts/chat.tsx
@@ -3,38 +3,22 @@ import React, { createContext, useContext, useState } from 'react'
 import { Message } from '../utils/interfaces'
 
 interface ChatContextData {
-  activeChat: Message[]
-  updateActiveChat(chat: Message[]): void
-  AddMessageToActiveChat(message: Message, contact: string): void
-  activeContact: string
-  updateActiveContact(contact: string): void
+  lastMessageReceived: Message
+  setLastMessageReceived(state: Message): void
 }
 
 const ChatContext = createContext<ChatContextData>({} as ChatContextData)
 
 const ChatProvider: React.FC = ({ children }) => {
-  const [activeChat, setActiveChat] = useState<Message[]>([])
-  const [activeContact, setActiveContact] = useState('')
-
-  const updateActiveChat = (chat: Message[]) => setActiveChat(chat)
-  const updateActiveContact = (contact: string) => setActiveContact(contact)
-
-  console.log(`active contact no chat context -${activeContact}-`)
-
-  const AddMessageToActiveChat = (message: Message, contact: string) => {
-    if (contact == activeContact) {
-      setActiveChat(prevState => [...prevState, message])
-    }
-  }
+  const [lastMessageReceived, setLastMessageReceived] = useState<Message>(
+    {} as Message,
+  )
 
   return (
     <ChatContext.Provider
       value={{
-        activeChat,
-        updateActiveChat,
-        AddMessageToActiveChat,
-        activeContact,
-        updateActiveContact,
+        lastMessageReceived,
+        setLastMessageReceived,
       }}>
       {children}
     </ChatContext.Provider>
@@ -42,19 +26,11 @@ const ChatProvider: React.FC = ({ children }) => {
 }
 
 export const useChat = () => {
-  const {
-    AddMessageToActiveChat,
-    activeChat,
-    updateActiveChat,
-    activeContact,
-    updateActiveContact,
-  } = useContext(ChatContext)
+  const { lastMessageReceived, setLastMessageReceived } =
+    useContext(ChatContext)
   return {
-    AddMessageToActiveChat,
-    activeChat,
-    updateActiveChat,
-    activeContact,
-    updateActiveContact,
+    lastMessageReceived,
+    setLastMessageReceived,
   }
 }
 
