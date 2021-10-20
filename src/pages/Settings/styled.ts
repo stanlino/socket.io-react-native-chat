a6e55918c54ce3b3d5f4dea124e8e0b4dfad76b9
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-19 23:42:03 -0300

    update settings page styles

--- src/pages/Settings/styled.ts
+++ src/pages/Settings/styled.ts
@@ -2,20 +2,20 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${props => props.theme.colors.Primary};
+    background-color: ${({ theme }) => theme.backgroundColor};
 `
 export const Tag = styled.Text`
-    font-size: ${props => props.theme.sizes.one * 6}px;
-    color: ${props => props.theme.colors.Secundary};
+    font-size: ${({ theme }) => theme.sizes.one * 6}px;
+    color: ${({ theme }) => theme.secundaryColor};
     font-weight: 500;
 `
 export const Section = styled.TouchableOpacity`
     flex-direction: row;
-    padding: ${props => props.theme.sizes.one * 3}px;
+    padding: ${({ theme }) => theme.sizes.one * 3}px;
     justify-content: space-between;
     align-items: center;
 `
 export const Output = styled.Text`
-    font-size: ${props => props.theme.sizes.one * 5}px;
-    color: ${props => props.theme.colors.Secundary};
+    font-size: ${({ theme }) => theme.sizes.one * 5}px;
+    color: ${({ theme }) => theme.secundaryColor};
 `
