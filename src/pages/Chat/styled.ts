a8c7bef583eba757a246574f97ea77233e129fd2
Stanley Filipe <stanley92filipe@gmail.com>
2021-10-19 23:41:04 -0300

    update chat page styles

--- src/pages/Chat/styled.ts
+++ src/pages/Chat/styled.ts
@@ -6,14 +6,14 @@ interface MessageProps {
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${props => props.theme.colors.Primary};
+    background-color: ${({ theme }) => theme.backgroundColor};
 `
 
 export const BottomBar = styled.View`
-    height: ${props => props.theme.sizes.ten * 1.5}px;
-    padding: ${props => props.theme.sizes.one * 1}px;
+    height: ${({ theme }) => theme.sizes.ten * 1.5}px;
+    padding: ${({ theme }) => theme.sizes.one * 1}px;
     flex-direction: row;
-    background-color:  ${props => props.theme.colors.Secundary};
+    background-color:  ${({ theme }) => theme.primaryColor};
     align-items: center;
 `
 
@@ -21,45 +21,45 @@ export const TextInput = styled.TextInput.attrs({
     placeholderTextColor: 'grey',
     placeholder: 'Escreva uma mensagem'
 })` 
-    height: ${props => props.theme.sizes.ten}px;
-    background-color: ${props => props.theme.colors.Primary};
-    border-radius: ${props => props.theme.sizes.ten}px;
+    height: ${({ theme }) => theme.sizes.ten}px;
+    background-color: ${({ theme }) => theme.primaryColor};
+    border-radius: ${({ theme }) => theme.sizes.ten}px;
     flex: 1;
-    color: ${props => props.theme.colors.Secundary};
-    font-size: ${props => props.theme.sizes.one * 4}px;
-    padding-left: ${props => props.theme.sizes.one * 3}px;
-    padding-right: ${props => props.theme.sizes.one * 3}px;
+    color: ${({ theme }) => theme.secundaryColor};
+    font-size: ${({ theme }) => theme.sizes.one * 4}px;
+    padding-left: ${({ theme }) => theme.sizes.one * 3}px;
+    padding-right: ${({ theme }) => theme.sizes.one * 3}px;
     font-weight: 400;
 `
 export const TouchableSend = styled.TouchableOpacity`
-    margin-left: ${props => props.theme.sizes.one}px;
+    margin-left: ${({ theme }) => theme.sizes.one}px;
     justify-content: center;
     align-items: center;
-    background-color: ${props => props.theme.colors.Metallic_Seaweed};
-    border-radius: ${props => props.theme.sizes.ten}px;
-    height: ${props => props.theme.sizes.ten}px;
-    width: ${props => props.theme.sizes.ten}px;
+    background-color: ${({ theme }) => theme.secundaryColor};
+    border-radius: ${({ theme }) => theme.sizes.ten}px;
+    height: ${({ theme }) => theme.sizes.ten}px;
+    width: ${({ theme }) => theme.sizes.ten}px;
 `
 
 export const MessageFatherView = styled.View<MessageProps>`
     flex-direction: row;
-    justify-content: ${props => props.fromThisUser ? 'flex-end' : 'flex-start'};
-    margin: ${props => props.theme.sizes.one}px;
+    justify-content: ${({ fromThisUser }) => fromThisUser ? 'flex-end' : 'flex-start'};
+    margin: ${({ theme }) => theme.sizes.one}px;
 `
 export const MessageChildView = styled.View<MessageProps>`
-    background-color: ${props => props.fromThisUser ? props.theme.colors.Lavender_Web : props.theme.colors.Beau_Blue};
-    min-height: ${props => props.theme.sizes.ten}px;
+    background-color: ${({ fromThisUser, theme }) => fromThisUser ? theme.colors.whiteblue : theme.colors.blue};
+    min-height: ${({ theme }) => theme.sizes.ten}px;
     justify-content: center;
-    padding: ${props => props.theme.sizes.one}px;
-    max-width: ${props => props.theme.sizes.ten * 9.5}px;
-    padding-left: ${props => props.theme.sizes.one * 2}px;
-    padding-right: ${props => props.theme.sizes.one * 2}px;
-    min-width: ${props => props.theme.sizes.ten * 1.5}px;
-    border-radius: ${props => props.theme.sizes.one * 3}px;
-    border-bottom-right-radius: ${props => props.fromThisUser ? 0 : props.theme.sizes.one * 3}px;
-    border-top-left-radius: ${props => props.fromThisUser ? props.theme.sizes.one * 3 : 0}px;
+    padding: ${({ theme }) => theme.sizes.one}px;
+    max-width: ${({ theme }) => theme.sizes.ten * 9.5}px;
+    padding-left: ${({ theme }) => theme.sizes.one * 2}px;
+    padding-right: ${({ theme }) => theme.sizes.one * 2}px;
+    min-width: ${({ theme }) => theme.sizes.ten * 1.5}px;
+    border-radius: ${({ theme }) => theme.sizes.one * 3}px;
+    border-bottom-right-radius: ${({ theme, fromThisUser }) => fromThisUser ? 0 : theme.sizes.one * 3}px;
+    border-top-left-radius: ${({ theme, fromThisUser }) => fromThisUser ? theme.sizes.one * 3 : 0}px;
 `
 export const Text = styled.Text`
-    font-size: ${props => props.theme.sizes.one * 4}px;
+    font-size: ${({ theme }) => theme.sizes.one * 4}px;
     font-weight: 600;
 `
